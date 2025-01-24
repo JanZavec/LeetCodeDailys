ec41c1448257906c25397a1fcb7640e79ffa46e1
JanZavec <34006173+JanZavec@users.noreply.github.com>
2025-01-24 21:10:54 +0100

    leetcode solved 1/365

---
+++ LeetCodeDailys/LeetCodeDailys/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Leetcode.Dailys;
+
+class Program
+{
+    private static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = [];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.TryGetValue(complement, out int value))
+            {
+                return [value, i ];
+            }
+            map[nums[i]] = i;
+        }
+
+        return [];
+    }
+
+
+    public static int Main(string[] args)
+    {
+        TwoSum([2, 7, 11, 15], 9);
+        return 0;
+    }
+}
+
+
+
+
