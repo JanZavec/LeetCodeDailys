b201bb983751c3342f3688545521d55b90dfa3e0
Jan <janzavec608@outlook.com>
2025-11-18 08:19:19 +0100

    chore: daily leetcode solution for 18/11

---
+++ LeetCodeDailys/LeetCodeDailys/Dailies/18.11.2025/Daily.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LeetCodeDailys.Dailies._18._11._2025;
+
+internal class Daily
+{
+    public bool IsOneBitCharacter(int[] bits)
+    {
+        for (int i = 0; i < bits.Length - 1; i++) 
+        {
+            if (bits[i] == 0)
+            {
+                continue;
+            }
+            else if (bits[i] == 1)
+            {
+                ++i;
+                if (i == bits.Length - 1) return false;
+            }
+        }
+        return true;
+    }
+}
