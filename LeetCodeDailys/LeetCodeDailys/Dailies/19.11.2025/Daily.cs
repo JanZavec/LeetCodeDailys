c6e38d685d232e99b4b5be0460c1199de117f36e
Jan <janzavec608@outlook.com>
2025-11-19 18:25:51 +0100

    chore: daily leetcode solution for 19/11

---
+++ LeetCodeDailys/LeetCodeDailys/Dailies/19.11.2025/Daily.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LeetCodeDailys.Dailies._19._11._2025;
+
+internal class Daily
+{
+    public int FindFinalValue(int[] nums, int original)
+    {
+        int multiplication = original;
+       var aa =  nums.ToList();
+        aa.Sort();
+        foreach (var num in  nums)
+        {
+            if (num == original) 
+            {
+                multiplication = 2 * num;
+                original = multiplication;
+            }
+        }
+        return multiplication;
+    }
+}
