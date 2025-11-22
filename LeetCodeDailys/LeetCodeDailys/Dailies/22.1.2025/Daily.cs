f9cca575e471a348468ae2cfa4d48c9a60536ee9
Jan <janzavec608@outlook.com>
2025-11-22 20:58:31 +0100

    chore: daily leetcode solution for 22/11

---
+++ LeetCodeDailys/LeetCodeDailys/Dailies/22.1.2025/Daily.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LeetCodeDailys.Dailies._22._11._2025;
+
+internal class Daily
+{
+    public int MinimumOperations(int[] nums)
+    {
+        return nums.Count(x => x % 3 != 0);
+    }
+}
