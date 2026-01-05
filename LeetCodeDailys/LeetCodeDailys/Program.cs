7dabe210405a1302a5134e36dac641450b2aba50
Jan <janzavec608@outlook.com>
2026-01-05 10:00:41 +0100

    chore: dailly neetcode ot keep me sane.

--- LeetCodeDailys/LeetCodeDailys/Program.cs
+++ LeetCodeDailys/LeetCodeDailys/Program.cs
@@ -8,24 +8,27 @@ class Program
 {
     private static int[] TwoSum(int[] nums, int target)
     {
-        Dictionary<int, int> map = [];
+        Dictionary<int, int> map = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
         {
             int complement = target - nums[i];
             if (map.TryGetValue(complement, out int value))
             {
-                return [value, i ];
+                var array = new int[2];
+                array[0] = value;
+                array[1] = i;
+                return array;
             }
             map[nums[i]] = i;
         }
 
-        return [];
+        return new int[1];
     }
 
 
     public static int Main(string[] args)
     {
-        Problems.MergeAlternately("abc", "pqr");
+        var aa = TwoSum([3,4,5,6], 7);
         return 0;
     }
 }
